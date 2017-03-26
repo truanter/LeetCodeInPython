8c2c15fe16036a61f3f0f05509712f4f4b5dea6a
976251185 <976251185@qq.com>
2017-03-26 16:01:04 +0800

    Update AddDigits.py

--- AddDigits.py
+++ AddDigits.py
@@ -11,3 +11,15 @@ class Solution(object):
         if sum >= 10:
             return self.addDigits(sum)
         return sum
+
+# Version 2 copied from others
+class Solution(object):
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        return num%9 if num%9 != 0 else 9
+ '''
+ 100a + 10b + c % 9
+ = 99a + a +9b + b +c %9
+ = a + b + c %9
+ '''
