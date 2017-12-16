11060ac1cce035233347be853cb0b0fbfad8c605
MRZ <976251185@qq.com>
2017-12-16 21:13:03 +0800

    Find Smallest Letter Greater Than Target

---
+++ smallest_letters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for i in letters:
+            if i > target:
+                return i
+        return letters[0]
