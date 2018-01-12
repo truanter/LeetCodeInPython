9eac594960b2d13c9d8a1354c687b1ee86c26236
976251185 <976251185@qq.com>
2018-01-12 13:43:33 +0800

    Find Anagram Mappings

---
+++ anagram_mappings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def anagramMappings(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        return [B.index(i) for i in A]
