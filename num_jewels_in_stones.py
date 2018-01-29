a7450842b8cc7ec3cbbfb65ca5a60b9df494195c
976251185 <976251185@qq.com>
2018-01-29 15:18:53 +0800

    Num Jewels in Stones

---
+++ num_jewels_in_stones.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for i in S:
+            if i in J:
+                count += 1
+
+        return count
